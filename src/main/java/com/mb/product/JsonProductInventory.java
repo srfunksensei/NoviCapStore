5d738fde462a2bc53134d21c203744d4e3417e51
MB <sr.funk.sensei@gmail.com>
2019-09-29 21:00:41 +0200

    added ProductInventory with it's implementation

---
+++ src/main/java/com/mb/product/JsonProductInventory.java
@@ -0,0 +1,41 @@
+package com.mb.product;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.json.simple.parser.ParseException;
+
+public class JsonProductInventory implements ProductInventory {
+    
+    private final Logger logger = Logger.getLogger(JsonProductFileReader.class.getName());
+
+    private JsonProductFileReader reader;
+
+    public JsonProductInventory(final String fileName) throws FileNotFoundException {
+        reader = new JsonProductFileReader(fileName);
+    }
+
+    public JsonProductInventory(final File file) throws FileNotFoundException {
+        reader = new JsonProductFileReader(file);
+    }
+    
+    public JsonProductInventory() throws FileNotFoundException {
+        final String fileName = getClass().getResource(JsonProductFileReader.PRODUCT_FILE_NAME).getFile();
+        reader = new JsonProductFileReader(fileName);
+    }
+
+    public Set<Product> getItems() {
+        try {
+            return reader.read();
+        } catch (ParseException e) {
+            logger.log(Level.WARNING, "Could not read json file");
+        }
+        
+        return Collections.emptySet();
+    }
+
+}
---
+++ src/main/java/com/mb/product/ProductInventory.java
@@ -0,0 +1,8 @@
+package com.mb.product;
+
+import java.util.Set;
+
+public interface ProductInventory {
+
+    Set<Product> getItems();
+}
